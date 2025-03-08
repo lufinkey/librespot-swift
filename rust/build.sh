592339a986c81e6808cbd198ec5690782deda34b
Luis Finke <luisfinke@gmail.com>
2025-03-08 16:19:04 -0500

    log each step of the build

--- rust/build.sh
+++ rust/build.sh
@@ -17,6 +17,8 @@ XCFRAMEWORK_HEADERS_DIR="include"
 export RUST_BACKTRACE=full
 
 cd "$(dirname "$0")" || exit $?
+
+>&2 echo "Building rust project"
 cargo build \
 	--target "x86_64-apple-ios" \
 	--target "aarch64-apple-ios" \
@@ -25,19 +27,25 @@ cargo build \
 	--target "aarch64-apple-darwin" \
 	--release || exit $?
 mkdir -p lib || exit $?
-# build combined ios simulator lib
+
+# create combined ios simulator lib
+>&2 echo "Creating combined iOS Simulator lib"
 mkdir -p lib/ios_simulator || exit $?
 if [ -f "lib/ios_simulator/$LIB_FILE" ]; then
 	rm -rf "lib/ios_simulator/$LIB_FILE" || exit $?
 fi
 lipo -create "target/x86_64-apple-ios/release/$LIB_FILE" "target/aarch64-apple-ios-sim/release/$LIB_FILE" -output "lib/ios_simulator/$LIB_FILE" || exit $?
-# build combined macos lib
+
+# create combined macos lib
+>&2 echo "Creating combined macOS lib"
 mkdir -p lib/macos || exit $?
 if [ -f "lib/macos/$LIB_FILE" ]; then
 	rm -rf "lib/macos/$LIB_FILE" || exit $?
 fi
 lipo -create "target/x86_64-apple-darwin/release/$LIB_FILE" "target/aarch64-apple-darwin/release/$LIB_FILE" -output "lib/macos/$LIB_FILE" || exit $?
-# build xcframework
+
+# create xcframework
+>&2 echo "Generating xcframework"
 if [ -d "lib/$XCFRAMEWORK_FILE" ]; then
 	rm -rf "lib/$XCFRAMEWORK_FILE" || exit $?
 fi
