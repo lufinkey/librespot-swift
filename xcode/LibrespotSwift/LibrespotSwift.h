ec95626aa929a496260cddd719fde38dc7d7bd0e
Luis Finke <luisfinke@gmail.com>
2025-03-09 18:25:14 -0400

    xcode project

---
+++ xcode/LibrespotSwift/LibrespotSwift.h
@@ -0,0 +1,20 @@
+//
+//  LibrespotSwift.h
+//  LibrespotSwift
+//
+//  Created by Luis Finke on 3/9/25.
+//
+
+#import <Foundation/Foundation.h>
+#import <LibrespotSwift/SwiftBridgeCore.h>
+#import <LibrespotSwift/respot_swift_gen.h>
+
+//! Project version number for LibrespotSwift.
+FOUNDATION_EXPORT double LibrespotSwiftVersionNumber;
+
+//! Project version string for LibrespotSwift.
+FOUNDATION_EXPORT const unsigned char LibrespotSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LibrespotSwift/PublicHeader.h>
+
+
